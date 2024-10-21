3cc276ff2d1518854573cd35f5c5c5ce8d1be609
yamarkz <y.kazuki0614n@gmail.com>
2024-10-21 21:25:57 +0900

    split file

---
+++ checkdigit.go
@@ -0,0 +1,32 @@
+package codabar
+
+import "strconv"
+
+type checkDigit string
+type CheckDigitStrategy func(seed) checkDigit
+
+func NewCheckDigitByMod11W7(seed seed) checkDigit {
+	num := uint64(seed)
+
+	var digits []uint64
+	for num > 0 {
+		digits = append([]uint64{num % 10}, digits...)
+		num /= 10
+	}
+
+	var sum uint64
+	weights := []uint64{2, 3, 4, 5, 6, 7}
+	weightIndex := 0
+	for i := len(digits) - 1; i >= 0; i-- {
+		sum += digits[i] * weights[weightIndex]
+		weightIndex = (weightIndex + 1) % len(weights)
+	}
+
+	mod := sum % 11
+	var value uint64
+	if mod != 0 && mod != 1 {
+		value = 11 - mod
+	}
+
+	return checkDigit(strconv.FormatUint(value, 10))
+}
--- codabar.go
+++ codabar.go
@@ -1,9 +1,5 @@
 package codabar
 
-import (
-	"strconv"
-)
-
 type codabar string
 
 func NewCodabar(prefix prefix, suffix suffix, body body) codabar {
@@ -17,33 +13,4 @@ func NewCodabarWithCheckDigit(prefix prefix, suffix suffix, body body, strategy 
 	}
 	checkDigit := strategy(seed)
 	return codabar(string(prefix) + string(body) + string(checkDigit) + string(suffix)), nil
-}
-
-type checkDigit string
-type CheckDigitStrategy func(seed) checkDigit
-
-func NewCheckDigitByMod11W7(seed seed) checkDigit {
-	num := uint64(seed)
-
-	var digits []uint64
-	for num > 0 {
-		digits = append([]uint64{num % 10}, digits...)
-		num /= 10
-	}
-
-	var sum uint64
-	weights := []uint64{2, 3, 4, 5, 6, 7}
-	weightIndex := 0
-	for i := len(digits) - 1; i >= 0; i-- {
-		sum += digits[i] * weights[weightIndex]
-		weightIndex = (weightIndex + 1) % len(weights)
-	}
-
-	mod := sum % 11
-	var value uint64
-	if mod != 0 && mod != 1 {
-		value = 11 - mod
-	}
-
-	return checkDigit(strconv.FormatUint(value, 10))
 }
