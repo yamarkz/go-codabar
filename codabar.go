4c9ff1a41424f0eb497f36411c551f35a6269e99
yamarkz <y.kazuki0614n@gmail.com>
2024-10-21 21:04:29 +0900

    Add prefix suffix validate value

--- codabar.go
+++ codabar.go
@@ -1,17 +1,28 @@
 package codabar
 
+import (
+	"fmt"
+	"strings"
+)
+
 type codabar string
 
 type prefix string
 type suffix string
 type body string
 
-func NewPrefix(value string) prefix {
-	return prefix(value)
+func NewPrefix(value string) (prefix, error) {
+	if len(value) != 1 || !strings.ContainsAny(value, "ABCD") {
+		return prefix(""), fmt.Errorf("prefix must be a single character")
+	}
+	return prefix(value), nil
 }
 
-func NewSuffix(value string) suffix {
-	return suffix(value)
+func NewSuffix(value string) (suffix, error) {
+	if len(value) != 1 || !strings.ContainsAny(value, "ABCD") {
+		return suffix(""), fmt.Errorf("suffix must be a single character")
+	}
+	return suffix(value), nil
 }
 
 func NewBody(value string) body {
