b982eb94ec698d26b54cae7ed20c8f245dad9d6b
yamarkz <y.kazuki0614n@gmail.com>
2024-10-21 21:01:57 +0900

    Add prefix suffix body

--- codabar.go
+++ codabar.go
@@ -2,6 +2,22 @@ package codabar
 
 type codabar string
 
-func NewCodabar() codabar {
-	return codabar("codabar")
+type prefix string
+type suffix string
+type body string
+
+func NewPrefix(value string) prefix {
+	return prefix(value)
+}
+
+func NewSuffix(value string) suffix {
+	return suffix(value)
+}
+
+func NewBody(value string) body {
+	return body(value)
+}
+
+func NewCodabar(prefix prefix, suffix suffix, body body) codabar {
+	return codabar(string(prefix) + string(body) + string(suffix))
 }
